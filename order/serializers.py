4ac9cfddeeb39399b141cd85e383ab3caa9ef4b1
brenopereira18 <brenopereira@gmail.com>
2024-11-04 14:47:21 -0300

    :sparkles: feat: customizes serializer

--- order/serializers.py
+++ order/serializers.py
@@ -1,16 +1,29 @@
 from rest_framework import serializers
 from product.models import Product
+from order.models import Order
 from product.serializers import ProductSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
     product = ProductSerializer(required=True, many=True)
+    products_id = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), write_only=True, many=True)
     total = serializers.SerializerMethodField()
 
     class Meta:
-        model = Product
-        fields = ['product', 'total']
+        model = Order
+        fields = ['product', 'total', 'user', 'products_id']
+        extra_kwargs = {'product': {'required': False}}
 
     def get_total(self, instance):
         total = sum([product.price for product in instance.product.all()])
         return total
+    
+    def create(self, validated_data):
+        product_data = validated_data.pop('products_id')
+        user_data = validated_data.pop('user')
+
+        order = Order.objects.create(validated_data)
+        for product in product_data:
+            order.product.add(product)
+
+        return order
--- product/serializers.py
+++ product/serializers.py
@@ -16,13 +16,25 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(required=True, many=True,)
+    categories_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), write_only=True, many=True)
 
     class Meta:
         model = Product
         fields = [
+            'id',
             'title',
             'description',
             'price',
             'active',
             'category',
+            'categories_id',
         ]
+
+    def create(self, validade_data):
+        category_data = validade_data.pop('categories_id')
+
+        product = Product.objects.create(**validade_data)
+        for category in category_data:
+            product.category.add(category)
+
+        return product
